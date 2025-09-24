d1271b642d2c5184c630622f04bc1abe1b4e0755
Ashish Kumar <kashish84396@gmail.com>
2025-09-24 19:17:23 +0530

    new questions

---
+++ practices/Hashmap/MajorityElement.java
@@ -0,0 +1,32 @@
+package practices.Hashmap;
+
+import java.util.HashMap;
+
+public class MajorityElement {
+
+  public static void findMajorityElements(int[] nums) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    int n = nums.length;
+
+    // Count frequency
+    for (int i = 0; i < n; i++) {
+      if (map.containsKey(nums[i])) { // if key exists
+        map.put(nums[i], map.get(nums[i]) + 1);
+      } else { // if key does not exist
+        map.put(nums[i], 1);
+      }
+    }
+
+    // Print elements appearing more than n/3 times
+    for (int key : map.keySet()) {
+      if (map.get(key) > n / 3) {
+        System.out.println(key);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] nums = { 1, 3, 2, 5, 1, 3, 1, 5, 1 };
+    findMajorityElements(nums);
+  }
+}
