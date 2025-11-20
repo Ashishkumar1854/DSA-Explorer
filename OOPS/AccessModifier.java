5ce96bdbdbb68bc8791a3245eb2b93cfa32302ba
Ashish Kumar <kashish84396@gmail.com>
2025-11-20 20:12:06 +0530

    setter

--- OOPS/AccessModifier.java
+++ OOPS/AccessModifier.java
@@ -1,3 +1,18 @@
 public class AccessModifier {
 
+  public static void main(String args[]) {
+    BankAccount myAcc = new BankAccount();
+    myAcc.username = "ashishyadav";
+    myAcc.setPassword("Ashish@2004");
+  }
+
+  class BankAccount {
+    public String username;
+    private String password;
+
+    public void setPassword(String pwd) {
+      password = pwd;
+    }
+  }
+
 }
